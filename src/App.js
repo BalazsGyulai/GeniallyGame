16533191622da4f5292b4f6231cde9388586c04e
BalazsGyulai <trombita0531@gmail.com>
2022-12-07 18:33:00 +0100

    yeaah

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import CorrectAnswer from "./components/CorrectAnswer";
 
 const App = (props) => {
+  let [questions, setQuestions] = useState([
+    {
+      quest: "Kinek az udvarában él együtt Kepler és Borbála?",
+      answer: "Rudolf",
+    },
+    {
+      quest: "Miből kell megélnie Keplernek?",
+      answer: "Kutatásaiból",
+    },
+    {
+      quest: "Mit kénytelen alkalmazni Kepler, hogy kutatásaiból megtudjon élni?",
+      answer: "Áltudományt",
+    },
+    {
+      quest: "Kivel tölti éjszakáit Éva?",
+      answer: "Udvaronccal",
+    },
+    {
+      quest: "Ki látogatja meg Keplert?",
+      answer: "Tanítványa",
+    },
+  ]);
+
+  const [round, setRound] = useState(0);
   const [word, setWord] = useState("");
   const [guess, setGuess] = useState([]);
   const [tip, setTip] = useState(0);
@@ -12,12 +37,17 @@ const App = (props) => {
     ["A", "S", "D", "F", "G", "H", "J", "K", "L", "É", "Á", "Ű"],
     ["Í", "Y", "X", "C", "V", "B", "N", "M", "Ö", "Ü"],
   ];
-  const [found, setFound] = useState(false);
+  const [found, setFound] = useState(0);
 
-
-  const szo = "ALMÁS";
   useEffect(() => {
-    setWord(szo);
+    MakeAnswersBigger();
+
+    initialiseGame();
+  }, [round]);
+
+  const initialiseGame = () => {
+
+    setWord(questions[round].answer);
 
     let array = [];
     let keyboardArray = [];
@@ -25,7 +55,7 @@ const App = (props) => {
     for (let i = 0; i < 5; i++) {
       let wordLen = [];
 
-      for (let j = 0; j < szo.length; j++) {
+      for (let j = 0; j < questions[round].answer.length; j++) {
         wordLen.push({
           value: "",
           color: "grey",
@@ -35,11 +65,9 @@ const App = (props) => {
       array.push(wordLen);
     }
 
-
-    for (let i = 0; i < keyboardLayout.length; i++){
+    for (let i = 0; i < keyboardLayout.length; i++) {
       let row = [];
-      for(let j = 0; j < keyboardLayout[i].length; j++){
-
+      for (let j = 0; j < keyboardLayout[i].length; j++) {
         row.push({
           value: keyboardLayout[i][j],
           color: "grey",
@@ -51,10 +79,19 @@ const App = (props) => {
 
     setKeyboard(keyboardArray);
     setGuess(array);
-  }, []);
+  };
 
+  const MakeAnswersBigger = () => {
+    let questionAnswer = questions;
+    for (let i = 0; i < questionAnswer.length; i++) {
+      questionAnswer[i].answer = questionAnswer[i].answer.toUpperCase();
+    }
+
+    setQuestions(questionAnswer);
+  };
 
   const KeyboardFunction = (e) => {
+
     let newState = guess;
 
     if (e.target.innerHTML === "DEL") {
@@ -70,60 +107,59 @@ const App = (props) => {
 
     setGuess(newState);
     IsANewWord(tip, progress + 1, newState);
+
+    e.target.classList.add("keyboardKeyeffect");
+    setTimeout(() => {
+      e.target.classList.remove("keyboardKeyeffect");
+    }, 500);
+
   };
 
   const IsANewWord = (tip, progress, word) => {
-    if (progress >= szo.length) {
+    if (progress >= questions[round].answer.length) {
       LettersPosition(word, tip, progress);
 
       setTip((oldState) => oldState + 1);
       setProgress(0);
-      setTip((oldState) => {
-        if (oldState + 1 > 5) {
-          return 0;
-        } else {
-          return oldState;
-        }
-      });
     }
   };
 
   const LettersPosition = (word, tip, progress) => {
-    
     let newState = guess;
-    
 
     //quess
-    for (let i = 0; i < szo.length; i++){
+    for (let i = 0; i < questions[round].answer.length; i++) {
       let j = 0;
-      while (j < szo.length && word[tip][i].value !== szo[j]) {
+      while (
+        j < questions[round].answer.length &&
+        word[tip][i].value !== questions[round].answer[j]
+      ) {
         j++;
       }
-      
-      
-      if (j >= szo.length){
+
+      if (j >= questions[round].answer.length) {
         newState[tip][i].color = "red";
-      } else{
+      } else {
         newState[tip][i].color = "yellow";
       }
     }
-    
-    for (let i = 0; i < szo.length; i++) {
 
-      if (word[tip][i].value === szo[i]) {
+    for (let i = 0; i < questions[round].answer.length; i++) {
+      if (word[tip][i].value === questions[round].answer[i]) {
         newState[tip][i].color = "green";
       }
     }
 
-
     //keyboard
     let keyboardNewState = keyboard;
-    for (let i = 0; i < newState[tip].length; i++){
-      // szo[i] === keybord betujevel
-      for (let k = 0; k < keyboardNewState.length; k++){
-        for (let j = 0; j < keyboardNewState[k].length; j++){
-          if (newState[tip][i].value === keyboardNewState[k][j].value){
-            if(keyboardNewState[k][j].color === "grey" || keyboardNewState[k][j].color === "yellow"){
+    for (let i = 0; i < newState[tip].length; i++) {
+      for (let k = 0; k < keyboardNewState.length; k++) {
+        for (let j = 0; j < keyboardNewState[k].length; j++) {
+          if (newState[tip][i].value === keyboardNewState[k][j].value) {
+            if (
+              keyboardNewState[k][j].color === "grey" ||
+              keyboardNewState[k][j].color === "yellow"
+            ) {
               keyboardNewState[k][j].color = newState[tip][i].color;
             }
           }
@@ -135,37 +171,65 @@ const App = (props) => {
 
     setKeyboard(keyboardNewState);
     setGuess(newState);
-
   };
 
   const FoundTheWord = (newState, tip) => {
-    let wordLen = szo.length;
+    let wordLen = questions[round].answer.length;
     let correct = 0;
 
-    for (let i = 0; i < wordLen; i++){
-      if (newState[tip][i].value === szo[i]){
+    for (let i = 0; i < wordLen; i++) {
+      if (newState[tip][i].value === questions[round].answer[i]) {
         correct++;
       }
     }
 
-    if (correct === wordLen){
-      setFound(true);
+    if (correct === wordLen) {
+      setFound(1);
     } else {
-      setFound(false);
+      if (tip === 4) {
+        setFound(2);
+      } else {
+        setFound(0);
+      }
     }
-  }
+  };
 
+  const NewGameHandler = (newGame) => {
+    setFound(0);
+    setProgress(0);
+    setTip(0);
+
+    setRound((round) => {
+      if (round >= 4){
+        return 0;
+      } else {
+        return round + 1;
+      }
+    });
+  };
 
   return (
     <>
-      <div>{word}</div>
-      <div>{tip}</div>
-      <div>{progress}</div>
+      {found === 1 || found === 2 ? (
+        <CorrectAnswer
+          newgame={(newGame) => NewGameHandler(newGame)}
+          answer={questions[round].answer}
+          found={found}
+          tip={tip}
+          round={round}
+        />
+      ) : (
+        ""
+      )}
+      <h1 className="question">{
+        questions[round].quest
+      }</h1>
       <div className="guesses">
         {guess.map((word, index) => (
           <div key={index} className="words">
             {word.map((letter, indexLetter) => (
-              <div className={`letter letter${letter.color}`} key={indexLetter}>
+              <div className={`letter letter${letter.color}`} key={indexLetter}
+              >
                 {letter.value}
               </div>
             ))}
